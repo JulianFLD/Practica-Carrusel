38b5b6f3e5b86aa3b3f33d0f4a2653f5d72bf2f4
JulianFLD <Julian.rosario@hotmail.com>
2023-06-03 22:17:31 -0400

    updated

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const elementosCarousel = document.querySelectorAll('.carousel');
+    M.Carousel.init(elementosCarousel, {
+       duration: 150, 
+        dist: -80,
+        shift: 5,
+        padding: 5,
+        numVisible: 5,
+        /* Puntos indicador;*/
+        indicators: true,
+        noWerap: false,
+
+
+
+
+    })
+});
+
+
